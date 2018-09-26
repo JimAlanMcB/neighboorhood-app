69532d75235c47b6f5d4ee6bd75a4152bb92ec8f
Jim Mcbrayer <jimm@dogtopia.local>
2018-09-26 00:27:53 -0700

    and adjusting css again

--- src/Nav.js
+++ src/Nav.js
@@ -9,7 +9,7 @@ class MobileNav extends Component {
     const button = document.getElementById("nav-button");
     const navArrow = document.getElementById("nav-arrow");
     const navMenu = !this.state.dropped
-      ? ((button.style.top = "50%"), (nav.style.height = "48%"), (this.setState({dropped: true})), (navArrow.innerText = "↑"))
+      ? ((button.style.top = "55%"), (nav.style.height = "48%"), (this.setState({dropped: true})), (navArrow.innerText = "↑"))
       : ((button.style.top = "26%"), (nav.style.height = "20%"), (this.setState({dropped: false})), (navArrow.innerText = "↓"));
       return navMenu
   };
